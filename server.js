10b3f46c047a3a37b4bc82d88e0bf1114354d135
melissa rice <rice.jean29@gmail.com>
2018-05-13 19:28:22 -0600

    version3

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path")
 
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-var app = express();
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname,"/public")));
